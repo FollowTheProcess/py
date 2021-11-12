f36bba2380d1a0c209c088b114ac511fbfe56cfb
Tom Fleet <tomfleet2018@gmail.com>
2021-11-12 17:55:35 +0000

    Implement a python version struct (#1)
    * Implement a Version struct to match python interpreters from filename
    
    * Increase test coverage

---
+++ pkg/py/version.go
@@ -0,0 +1,63 @@
+package py
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const (
+	pythonExePrefix = "python"
+)
+
+// Version represents a version of a python interpreter
+// only major and minor are included because this is how the executables
+// are stored on disk (e.g. /usr/local/bin/python3.9)
+type Version struct {
+	Major int
+	Minor int
+}
+
+// FromFileName extracts the version information from a python interpreter's filename
+// and loads the information into the calling `Version`
+// If the filename does not start with `python` or does not have a valid
+// two digit version after it, an error will be returned
+//
+// A valid filename will look like `python3.9` or `python3.10`
+func (v *Version) FromFileName(filename string) error {
+	// Make sure the filename starts with `python`
+	if !strings.HasPrefix(filename, pythonExePrefix) {
+		return fmt.Errorf("filename is not a valid python interpreter: %s", filename)
+	}
+
+	// Because we know the filename will always start with `python`
+	// we can split off the version simply by indexing from the end of `python`
+	// to the end of the string
+	// This is a naive index that doesn't take UTF-8 runes into account but
+	// since this is a filepath I think that's pretty safe
+	version := filename[len(pythonExePrefix):]
+
+	parts := strings.Split(version, ".")
+
+	// If we can't get a part either side of a ".", we have a bad version
+	if len(parts) != 2 {
+		return fmt.Errorf("malformed interpreter version: %s from filename: %s", version, filename)
+	}
+
+	major, minor := parts[0], parts[1]
+
+	majorInt, err := strconv.Atoi(major)
+	if err != nil {
+		return fmt.Errorf("filename %s major version component could not be parsed as an int: %v", filename, major)
+	}
+
+	minorInt, err := strconv.Atoi(minor)
+	if err != nil {
+		return fmt.Errorf("filename %s minor version component could not be parses as an int: %v", filename, minor)
+	}
+
+	v.Major = majorInt
+	v.Minor = minorInt
+
+	return nil
+}
---
+++ pkg/py/version_test.go
@@ -0,0 +1,107 @@
+package py
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestVersion_FromFileName(t *testing.T) {
+	type args struct {
+		filename string
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    Version
+		wantErr bool
+	}{
+		{
+			name:    "valid 3.7",
+			args:    args{filename: "python3.7"},
+			want:    Version{Major: 3, Minor: 7},
+			wantErr: false,
+		},
+		{
+			name:    "valid 3.10",
+			args:    args{filename: "python3.10"},
+			want:    Version{Major: 3, Minor: 10},
+			wantErr: false,
+		},
+		{
+			name:    "valid 4.0",
+			args:    args{filename: "python4.0"},
+			want:    Version{Major: 4, Minor: 0},
+			wantErr: false,
+		},
+		{
+			name:    "valid 2.7",
+			args:    args{filename: "python2.7"},
+			want:    Version{Major: 2, Minor: 7},
+			wantErr: false,
+		},
+		{
+			name:    "no version numbers",
+			args:    args{filename: "python"},
+			want:    Version{},
+			wantErr: true,
+		},
+		{
+			name:    "just the major version (should never happen)",
+			args:    args{filename: "python3"},
+			want:    Version{},
+			wantErr: true,
+		},
+		{
+			name:    "filename not python",
+			args:    args{filename: "dingle3.7"},
+			want:    Version{},
+			wantErr: true,
+		},
+		{
+			name:    "bad major component",
+			args:    args{filename: "pythonp.7"},
+			want:    Version{},
+			wantErr: true,
+		},
+		{
+			name:    "bad minor component",
+			args:    args{filename: "python3.f"},
+			want:    Version{},
+			wantErr: true,
+		},
+		{
+			name:    "whitespace before",
+			args:    args{filename: " python 3.7 "},
+			want:    Version{},
+			wantErr: true,
+		},
+		{
+			name:    "whitespace between python and version",
+			args:    args{filename: "python 3.7"},
+			want:    Version{},
+			wantErr: true,
+		},
+		{
+			name:    "whitespace after",
+			args:    args{filename: "python 3.7 "},
+			want:    Version{},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := Version{}
+			err := v.FromFileName(tt.args.filename)
+
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("FromFileName() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if !reflect.DeepEqual(v, tt.want) {
+				t.Errorf("got %#v, wanted %#v", v, tt.want)
+			}
+		})
+	}
+}
